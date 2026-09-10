999e93f0d0185ad610392f1876dffaaaea7834a5
python-mxnt <verxateam@gmail.com>
2026-09-10 18:18:24 +0100

    fix attempt 2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+// Keep the Express import in the root entrypoint so Vercel's framework
+// detector recognizes this wrapper as the application entrypoint.
+require("express");
+
 module.exports = require("./server/index");
